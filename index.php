2f78e168080c3c5e99d963299afb9938e49c2a89
Kaustubh Utturwar <k.utturwar@gmail.com>
2021-08-31 20:35:59 +0530

    update

--- index.php
+++ index.php
@@ -60,6 +60,10 @@ include '_dbconnect.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="ATG-World">
+    <meta property="og:description" content="Responsive blogging website frontend">
+    <meta property="og:image" content="https://i.imgur.com/qnuCS07.png">
+    <meta property="og:url" content="https://world-atg.000webhostapp.com">
     <title>Home</title>
     <link rel="icon" href="logos/logo.png" type="image/icon type">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
